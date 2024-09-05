9b0427145dfd05ac5154b9e843bf99cacf4bbf44
muhammetakkan <muhammettakkann@gmail.com>
2024-09-05 11:23:15 +0300

    JWT create metotu yazıldı ve denendi

--- ApiCourseSltn2/Controllers/UsersController.cs
+++ ApiCourseSltn2/Controllers/UsersController.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ApiCourseSltn2.Controllers
 {
@@ -31,7 +34,7 @@ namespace ApiCourseSltn2.Controllers
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
-
+            
             var isUsers = await _userManager.Users.Select(i=> new {i.FullName, i.Email}).ToListAsync();
 
             if (isUsers is null || !isUsers.Any())
@@ -64,6 +67,7 @@ namespace ApiCourseSltn2.Controllers
                 DateAdded = DateTime.Now,
             };
 
+            //bu metot userName i ve Email i aynı yapmana engel oluyor.
             var result = await _userManager.CreateAsync(user, model.Password ?? "12345");
 
             if (result.Succeeded)
@@ -95,7 +99,7 @@ namespace ApiCourseSltn2.Controllers
                 //token oluşturma JWT
                 return Ok(new 
                 {
-                    token = "Token Gönderildi"
+                    token = GenerateJWT(user),
                 });
             }
 
@@ -105,11 +109,52 @@ namespace ApiCourseSltn2.Controllers
 
         //JWT Methot at here write
         //YAZILMAYA DEVAM EDİLECEK, ŞUAN EKSİK. => 04.09.2024
-        private object GenerateJWT(AppUser appUser)
+        private object GenerateJWT(AppUser user)
         {
+            
+            /*
+             * AppSettings i biz kendi isim terciğimizle AppSettings.Development.json a yazdık.
+             * Secret adlı isimlendirmeyi de aynı şekilde kendimiz yaptık.
+             * Encoding.ASCII.GetBytes => alınan/gelen(get) string anahtarı 
+            */
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Secret").Value ?? "");
+            
+            //tokenDescriptor => token tanımlayıcı(kimlik)
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                //Token ın özneleri => Subject
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    //Id ye denk geliyor lakin NameIdentifier(source a bak) string olduğundan kendi user.Id mizi de string e çeviriyoruz.
+                    new (ClaimTypes.NameIdentifier  , user.Id.ToString()), 
+
+                    //login vb durumlar için userName i kullandığımızdan ClamiTypes ı userName ile ilişkilendirdik.
+                    new (ClaimTypes.Name, user.UserName ?? ""),
+                }),
+
+                //Expires = süresi dolma
+                //Expires satırında jwt nin süresini belirliyoruz.
+                Expires = DateTime.UtcNow.AddDays(1), //1 gün sonra süresi bitecek
+
+                //JWT nin kimlik bilgilerini içerir. Yapısını ve şifrelenme türünü içerir.
+                //Önemli olarak SigningCredentials jwt güvenlidir, değiştirilmedi. Bu algoritma ve imza     ile de teyit ediyorum demektir. 
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256),
+                //SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256, "RS256") vb de kullanılabilirdi.
+            };
+
+            /*
+             * bir token üretebilmem için => 1 adet token yöneticisine(token handler)
+             *                            => 1 adet token kimlik bilgileri(tokenDescriptor)
+             * bir token ı kullanabilme için => 1 adet oluşturulmuş token ı yazdırmalı ve kullanıma göre(return vb) almalıyım.
+             */
+
             //token:token && handler:yönetici
             var tokenHandler = new JwtSecurityTokenHandler();
-            return tokenHandler;
+
+
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+            var finalyTokenProsess = tokenHandler.WriteToken(token);
+            return finalyTokenProsess;
         }
 
     }
